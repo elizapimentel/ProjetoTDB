c03462fa0ba307131cbc38826da25571905d6a0d
elizapimentel <elizapimentel@hotmail.com>
2022-08-16 23:43:05 -0300

    :bug: fix login page

---
+++ src/access/ConectaDB.java
@@ -0,0 +1,20 @@
+package access;
+
+public class ConectaDB {
+
+	public static void main(String[] args) {
+		BDLogin sintaxeBanco = new BDLogin();
+		sintaxeBanco.conectar();
+
+		if(sintaxeBanco.estaConectado()) {
+			sintaxeBanco.criarUsuario("admin", "admin");
+
+			System.out.println("Banco conectado");
+		}else {
+			System.out.println("Não foi possível conectar ao banco");
+		}
+
+
+	}
+
+}
